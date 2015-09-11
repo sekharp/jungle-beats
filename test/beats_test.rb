940a5997504703cc215a23f910e15c687392b5fb
Sekhar Paladugu <sekharp@gmail.com>
2015-09-11 08:37:28 -0600

    Final commit

--- test/beats_test.rb
+++ test/beats_test.rb
@@ -5,7 +5,7 @@ require './lib/beats'
 
 class BeatsTest < Minitest::Test
 
-  def test_it_plays_beats_txt_file
+  def test_it_says_it_plays_beats_txt_file
     skip
   end
 
@@ -14,35 +14,35 @@ class BeatsTest < Minitest::Test
   end
 
   def test_JungleBeat_appends_beats
-
+    skip
   end
 
   def test_JungleBeat_checks_list_for_all
-
+    skip
   end
 
   def test_JungleBeat_prepends_to_list
-
+    skip
   end
 
   def test_JungleBeat_pops_from_list_once
-
+    skip
   end
 
   def test_JungleBeat_pops_from_list_multiple_times
-
+    skip
   end
 
   def test_JungleBeat_inserts_to_list
-
+    skip
   end
 
   def test_JungleBeat_checks_includes_on_list
-
+    skip
   end
 
   def test_JungleBeat_counts_list
-
+    skip
   end
 
 end
